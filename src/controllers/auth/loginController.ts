c4de1ebeb7a4ad7494d2efbb853779c36edac381
Ram Krishna Yadav <itsramky234@gmail.com>
2025-12-19 15:07:52 +0545

    fixed production env

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -20,7 +20,8 @@ const loginController = async ({ body, set, cookie }: Context) => {
         }
         const accessToken = await generateAccessToken(user[0].id.toString());
         const refreshToken = await generateRefreshToken(user[0].id.toString());
-        const isProd = process.env.NODE_ENV === 'production';
+        const isProd = process.env.NODE_ENV == 'production';
+        console.log("Is Production Environment:", isProd);
         cookie.refreshToken.set({
             value: refreshToken,
             httpOnly: true,
